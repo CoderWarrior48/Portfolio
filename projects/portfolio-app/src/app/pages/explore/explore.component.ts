b22d825da930feba839bad24994126d28cf51b4d
CoderWarrior48 <dawson@hamera.com>
2023-11-03 11:41:59 -0700

    5 new changes

--- projects/portfolio-app/src/app/pages/explore/explore.component.ts
+++ projects/portfolio-app/src/app/pages/explore/explore.component.ts
@@ -6,5 +6,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./explore.component.scss']
 })
 export class ExploreComponent {
-
+  projects = [
+    {
+      name: 'Del Oro App',
+      tags: [
+        'Angular',
+        'HTML',
+        'Typescript',
+        'Nodejs',
+      ],
+      image: 'assets/images/DelOroApp-Screenshot.png',
+      path: 'apps/deloroapp' 
+    },
+    {
+      name: 'CogniQuest',
+      tags: [
+        'Angular',
+        'HTML',
+        'Typescript',
+        'Nodejs',
+      ],
+      image: 'assets/images/none',
+      path: 'apps/cogniquest' 
+    },
+    {
+      name: 'Decoder/Encoder',
+      tags: [
+        'Python',
+      ],
+      image: 'assets/images/none',
+      path: 'apps/decoder-encoder' 
+    },
+    {
+      name: 'Del Oro App',
+      tags: [
+        'Angular',
+        'HTML',
+        'Typescript',
+        'Nodejs',
+      ],
+      image: 'assets/images/DelOroApp-Screenshot.png',
+      path: 'apps/deloroapp' 
+    },
+    {
+      name: 'CogniQuest',
+      tags: [
+        'Angular',
+        'HTML',
+        'Typescript',
+        'Nodejs',
+      ],
+      image: 'assets/images/none',
+      path: 'apps/cogniquest' 
+    },
+    {
+      name: 'Decoder/Encoder',
+      tags: [
+        'Python',
+      ],
+      image: 'assets/images/none',
+      path: 'apps/decoder-encoder' 
+    }
+  ]
 }
