5df4d29acdf549aa6795ec21309b557092278bf7
CoderWarrior48 <dawson@hamera.com>
2023-11-29 07:52:01 -0800

    1 change

--- projects/portfolio-app/src/app/pages/project/project.component.ts
+++ projects/portfolio-app/src/app/pages/project/project.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy  } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit, OnDestroy {
+
+  id: any
+  private sub: any;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.id = params['id']; // (+) converts string 'id' to a number
+       
+       // In a real app: dispatch action to load the details here.
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 
 }
