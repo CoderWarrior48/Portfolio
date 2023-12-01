370e8f78756f1305f2414ae3d881e0618f382a51
CoderWarrior48 <77896436+CoderWarrior48@users.noreply.github.com>
2023-12-01 20:13:34 +0000

    1 change

--- projects/portfolio-app/src/app/pages/project/project.component.ts
+++ projects/portfolio-app/src/app/pages/project/project.component.ts
@@ -15,9 +15,8 @@ export class ProjectComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
-       this.id = params['id']; // (+) converts string 'id' to a number
-       
-       // In a real app: dispatch action to load the details here.
+       this.id = params['id'];
+    console.log(this.id)
     });
   }
 
