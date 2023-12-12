14d693e5231cc6dcb03546bb9df9dbcffc8cfbc5
CoderWarrior48 <77896436+CoderWarrior48@users.noreply.github.com>
2023-12-12 23:12:39 +0000

    Added ClimateChangeApp

---
+++ projects/climate-change-app/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { NavbarComponent } from 'libs/ui/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    NavbarComponent,
+    RouterModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ projects/climate-change-app/src/app/pages.data.ts
@@ -0,0 +1,7 @@
+export const pages = [
+    {name:'Home', path:'/home'},
+    {name:'About', path:'/about'},
+    {name:'Explore', path:'/explore'},
+    {name:'Impact', path:'/impact'}
+
+]
