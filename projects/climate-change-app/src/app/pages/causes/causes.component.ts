09a0d19698597c0f1eb36e3bf6b7f25420314ea1
CoderWarrior48 <77896436+CoderWarrior48@users.noreply.github.com>
2023-12-14 16:25:04 +0000

    new changes

--- projects/climate-change-app/src/app/pages/causes/causes.component.ts
+++ projects/climate-change-app/src/app/pages/causes/causes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-causes',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class CausesComponent {
 
+  @ViewChild('myElement') set(el) {
+    if (el) {
+      console.log('element loaded!');
+    }
+  }
+
+  scrollToElementById(id: string) {
+    let element = this.__getElementById(id);
+    this.scrollToElement(element);
+  }
+
+  private __getElementById(id: string): HTMLElement {
+    console.log("element id : ", id);
+    let element: any = document.getElementById(id);
+    return element;
+  }
+
+  scrollToElement(element: HTMLElement) {
+    element.scrollIntoView({ behavior: "smooth", block: 'center' });
+    console.log('element:', element)
+  }
 }
