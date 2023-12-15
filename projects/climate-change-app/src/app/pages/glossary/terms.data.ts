d332c1b3b65ed2f58bb85f80c8709471d57188a2
CoderWarrior48 <77896436+CoderWarrior48@users.noreply.github.com>
2023-12-15 01:48:38 +0000

    added to terms.data

--- projects/climate-change-app/src/app/pages/glossary/terms.data.ts
+++ projects/climate-change-app/src/app/pages/glossary/terms.data.ts
@@ -1,27 +1,38 @@
 export const terms = [
-    {
-      term: 'climate',
-      definition: 'The average weather in an area.',
-      link: 'climate'
-    },
-    {
-      term: 'weather',
-      definition: 'Conditions such as humitity, temperature...',
-      link: 'weather'
-    },
-    {
-      term: 'greenhouse gases',
-      definition: 'A gas that contributes to the greenhouse effect by absorbing infrared radiation.',
-      link: 'greenhousegases'
-    },
-    {
-      term: 'climate change',
-      definition: 'Come up with a definition...',
-      link: 'climatechange'
-    },
-    {
-      term: 'global warming',
-      definition: 'The global rise of average temperatures.',
-      link: 'globalwarming'
-    },
+  {
+    term: 'climate',
+    definition: 'The average weather conditions in an area in general or over a long period.',
+    link: 'climate'
+  },
+  {
+    term: 'weather',
+    definition: 'The state of the atmosphere at a place and time as regards temperature, humidity, sunlight, wind, or precipitation',
+    link: 'weather'
+  },
+  {
+    term: 'greenhouse gases',
+    definition: 'Any gases that contribute to the greenhouse effect by absorbing infrared radiation.',
+    link: 'greenhousegases'
+  },
+  {
+    term: 'climate change',
+    definition: 'Current and future changes in the Earth’s climate caused by global warming.',
+    link: 'climatechange'
+  },
+  {
+    term: 'global warming',
+    definition: "The long-term heating of Earth's surface observed since the pre-industrial period.",
+    link: 'globalwarming'
+  },
+  {
+    term: 'carbon tax',
+    definition: 'A proposed tax on all carbon producers in hopes to reduce its use.',
+    link: 'carbontax'
+  },
+{
+    term: 'permafrost',
+    definition: 'A thick subsurface layer of soil that remains frozen throughout the year, mainly found in polar regions.',
+    link: 'permafrost'
+  },
+
 ]
